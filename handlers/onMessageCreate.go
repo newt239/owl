b0a52ca365ca20406b804dccbd49d2b0ddc09d43
newt239 <ysti2019.win2020.st@gmail.com>
2022-08-12 21:28:20 +0900

    feature: clip youtube movie to music database in notion when send youtube url

--- handlers/onMessageCreate.go
+++ handlers/onMessageCreate.go
@@ -2,23 +2,75 @@ package handlers
 
 import (
 	"context"
-	"log"
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/jomei/notionapi"
+	"google.golang.org/api/option"
+	"google.golang.org/api/youtube/v3"
 )
 
 func OnMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	clientId := os.Getenv("CLIENT_ID")
 	u := m.Author
 	if u.ID != clientId {
-		client := notionapi.NewClient(notionapi.Token(os.Getenv("NOTION_API_KEY")))
-		database, _ := client.Database.Get(context.Background(), notionapi.DatabaseID(os.Getenv("MUSIC_DATABASE_ID")))
+		if strings.HasPrefix(m.Content, "https://") {
+			client := notionapi.NewClient(notionapi.Token(os.Getenv("NOTION_API_KEY")))
+			database, _ := client.Database.Get(context.Background(), notionapi.DatabaseID(os.Getenv("MUSIC_DATABASE_ID")))
+			arr := strings.Split(m.Content, " ")
 
-		_, err := s.ChannelMessageSend(m.ChannelID, database.URL)
-		if err != nil {
-			log.Println("Error sending message: ", err)
+			service, _ := youtube.NewService(context.Background(), option.WithAPIKey(os.Getenv("YOUTUBE_API_KEY")))
+			videoList, _ := service.Videos.List([]string{"snippet"}).Id(strings.Split(arr[0], "?v=")[1]).Do()
+			video := videoList.Items[0]
+			fmt.Println(video.Snippet.Description)
+			requestBody := notionapi.PageCreateRequest{
+				Parent: notionapi.Parent{
+					Type:       notionapi.ParentTypeDatabaseID,
+					DatabaseID: notionapi.DatabaseID(database.ID),
+				},
+				Properties: notionapi.Properties{
+					"Name": notionapi.TitleProperty{
+						Title: []notionapi.RichText{
+							{Text: notionapi.Text{Content: video.Snippet.Title}},
+						},
+					},
+					"URL": notionapi.URLProperty{
+						URL: arr[0],
+					},
+				},
+				Children: []notionapi.Block{
+					notionapi.ParagraphBlock{
+						BasicBlock: notionapi.BasicBlock{
+							Object: "block",
+							Type:   "paragraph",
+						},
+						Paragraph: notionapi.Paragraph{
+							RichText: []notionapi.RichText{
+								{Text: notionapi.Text{Content: video.Snippet.Description}},
+							},
+						},
+					},
+				},
+				Icon: &notionapi.Icon{
+					Type: "external",
+					External: &notionapi.FileObject{
+						URL: "https://www.youtube.com/s/desktop/e06db45c/img/favicon_144x144.png",
+					},
+				},
+				Cover: &notionapi.Image{
+					Type: "external",
+					External: &notionapi.FileObject{
+						URL: video.Snippet.Thumbnails.Maxres.Url,
+					},
+				},
+			}
+			page, err := client.Page.Create(context.Background(), &requestBody)
+			if err != nil {
+				fmt.Println(err)
+			}
+			s.ChannelMessageSend(m.ChannelID, page.URL)
 		}
 	}
 }
