9ab7e2cfc1fae87083e87a38e7dcb242c30f1d08
newt239 <ysti2019.win2020.st@gmail.com>
2022-08-12 18:54:37 +0900

    add: connect to notion

--- handlers/onMessageCreate.go
+++ handlers/onMessageCreate.go
@@ -1,19 +1,22 @@
 package handlers
 
 import (
-	"fmt"
+	"context"
 	"log"
 	"os"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/jomei/notionapi"
 )
 
 func OnMessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	clientId := os.Getenv("CLIENT_ID")
 	u := m.Author
-	fmt.Printf("%20s %20s(%20s) > %s\n", m.ChannelID, u.Username, u.ID, m.Content)
 	if u.ID != clientId {
-		_, err := s.ChannelMessageSend(m.ChannelID, "Hi!")
+		client := notionapi.NewClient(notionapi.Token(os.Getenv("NOTION_API_KEY")))
+		database, _ := client.Database.Get(context.Background(), notionapi.DatabaseID(os.Getenv("MUSIC_DATABASE_ID")))
+
+		_, err := s.ChannelMessageSend(m.ChannelID, database.URL)
 		if err != nil {
 			log.Println("Error sending message: ", err)
 		}
